85e3e9729cd135ab720db459b7192a4e2ed3cf1e
angelicapulido <angelica.rules73@gmail.com>
2021-09-19 21:35:04 -0600

    practica3

---
+++ sobrecargaConstructor.ts
@@ -0,0 +1,11 @@
+class punto{
+    x:number;
+    y:number;
+    s:string;
+    
+    constructor(x:number, y:number);
+    constructor(s:string);
+    constructor(xs:number | string, y?:number){
+       
+    }
+}
